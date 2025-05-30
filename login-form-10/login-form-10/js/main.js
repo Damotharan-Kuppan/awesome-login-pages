0735e22480b2eaf761f0dbae21d1b346c2395f71
Damotharan-Kuppan <50075141+Damotharan-Kuppan@users.noreply.github.com>
2025-05-30 18:34:55 +0530

    Change attribute name in form 10

--- login-form-10/login-form-10/js/main.js
+++ login-form-10/login-form-10/js/main.js
@@ -6,10 +6,10 @@
 
   $(this).toggleClass("fa-eye fa-eye-slash");
   var input = $($(this).attr("toggle"));
-  if (input.attr("type") == "password") {
-    input.attr("type", "text");
+  if (input.attr("type_1") == "password") {
+    input.attr("type_1", "text");
   } else {
-    input.attr("type", "password");
+    input.attr("type_1", "password");
   }
 });
 
